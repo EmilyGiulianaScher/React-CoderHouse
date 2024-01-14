22d4dcb0c66b126b319858f6c46dde4702f8531a
giuliana scher <emilygiulianascher@gmail.com>
2024-01-14 11:27:17 -0300

    Entrega Proyecto Final CoderHouse Scher Emily Giuliana 1.2

--- FrizaditasReactApp/vite.config.js
+++ FrizaditasReactApp/vite.config.js
@@ -6,3 +6,12 @@ module.exports = {
   },
   plugins: [],
 }
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/React-CoderHouse/",
+});
